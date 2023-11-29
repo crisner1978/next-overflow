fdb010a34ee00f48e29afe92e8c394dcbf3b1817
Chris Risner <chris@driv.ly>
2023-11-28 20:07:58 -0600

    feat(Layout): added assets and layout

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import { ClerkProvider } from '@clerk/nextjs'
 
-const Provider = ({ children }: {children: React.ReactNode}) => {
-  return <ClerkProvider>{children}</ClerkProvider>
+const Provider = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <ClerkProvider
+      appearance={{
+        elements: {
+          formButtonPrimary: 'primary-gradient',
+          footerActionLink: 'primary-text-gradient hover:text-primary-500',
+        },
+      }}>
+      {children}
+    </ClerkProvider>
+  )
 }
 
 export default Provider
